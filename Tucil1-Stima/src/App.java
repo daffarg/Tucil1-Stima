ce8a800791a3622489568273f13bdfd946ed6072
Mohamad Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2022-01-21 00:41:20 +0700

    finishing reading method from file

--- Tucil1-Stima/src/App.java
+++ Tucil1-Stima/src/App.java
@@ -5,5 +5,7 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         String filename = scanner.nextLine();
         WordSearchPuzzle x = new WordSearchPuzzle(filename);
+        System.out.println(x.alphabet);
+        System.out.println(x.word);
     }
 }
--- Tucil1-Stima/src/WordSearchPuzzle.java
+++ Tucil1-Stima/src/WordSearchPuzzle.java
@@ -8,36 +8,26 @@ import java.util.Scanner;
 public class WordSearchPuzzle {
     ArrayList<ArrayList<String>> alphabet;
     ArrayList<String> word;
-    // isi dari matriks. cara mengakses matriks Matrix.Content[row][col]
-    // indeks (row) baris dan (col) dimulai dari 0;
 
     // Untuk membaca word search puzzle dari file teks
     WordSearchPuzzle(String fileName) throws FileNotFoundException{
-        ArrayList<ArrayList<String>> alphabet = new ArrayList<ArrayList<String>>();
+        this.alphabet = new ArrayList<ArrayList<String>>();
+        this.word = new ArrayList<String>();
         File file = new File(fileName);
         Scanner scan = new Scanner(file);
-
-        String newLine = System.getProperty("line.separator");
-
+        
         while (scan.hasNextLine()) {
-            String[] a = scan.nextLine().split("\\s+");
-            List <String> tmp2 = (List<String>) Arrays.asList(a);
-            ArrayList<String> tmp = new ArrayList<String>(tmp2);
-            alphabet.add(tmp);
+            String next = scan.nextLine();
+            if (next.isEmpty()) { // membaca kunci jawaban (setelah line kosong)
+                while (scan.hasNextLine()) {
+                    this.word.add(scan.next());
+                }
+            } else { // membaca puzzle
+                String[] a = next.split("\\s+");
+                List <String> tmp2 = (List<String>) Arrays.asList(a);
+                ArrayList<String> tmp = new ArrayList<String>(tmp2);
+                this.alphabet.add(tmp);
+            }         
         }
-        System.out.println(alphabet);
-        // if (row == 0) {
-        //     System.out.println("Tidak ada data dalam file " + fileName);
-        // } else {
-        //     col = Content.get(0).size();
-        //     this.Content = new double[row][col];
-        //     this.RowEff = row;
-        //     this.ColEff = col;
-        //     for (int i = 0; i < row; i ++) {
-        //         for (int j = 0; j < col; j ++) {
-        //             this.Content[i][j] = Content.get(i).get(j);
-        //         }
-        //     }
-        // }
     }
 }
